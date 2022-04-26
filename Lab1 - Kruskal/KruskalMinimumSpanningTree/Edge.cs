270e2abec810b604b639dfc6cf31399ffd0bb971
Javiendha <91025317+Javiendha@users.noreply.github.com>
2022-04-26 13:46:15 +0300

    Add files via upload

---
+++ Lab1 - Kruskal/KruskalMinimumSpanningTree/Edge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kruskal
+{
+    public class Edge
+    {
+        public int StartN { get; set; }
+        public int EndN { get; set; }
+        public double Weight { get; set; }
+    }
+}
